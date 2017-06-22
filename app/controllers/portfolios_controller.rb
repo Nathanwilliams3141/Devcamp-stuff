8e905410534df98ba3ca9b87b69b841ebe818465
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:42:48 +0000

    Made portfolio edit stuff

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,16 +9,22 @@ class PortfoliosController < ApplicationController
   end
   
   def edit
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def create
     @portfolio_item = Portfolio.new( params.require(:portfolio).permit(:title, :subtitle, :body))
+  end
 
+   
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
     respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Your record was successfully updated.' }
       else
-        format.html { render :new }
+        format.html { render :edit }
       end
     end
   end
