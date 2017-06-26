228f8b48c77ae8d443c98a48c139dbd1e6559582
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 17:48:10 +0000

    Customized portfolio routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -15,9 +15,17 @@ class PortfoliosController < ApplicationController
   def show
      @portfolio_item = Portfolio.find(params[:id])
   end
-
+  
   def create
-    @portfolio_item = Portfolio.new( params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
   
   def destroy
