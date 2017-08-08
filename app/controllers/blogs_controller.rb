76e8af657096403ac1d80e706bb00f43b9e243d9
Jordan Hudgens <jordan@devcamp.com>
2017-08-08 16:38:41 +0000

    Implemented markdown on blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -56,7 +56,7 @@ class BlogsController < ApplicationController
   # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
-      if @blog.update(blog_params)
+      if @blog.update!(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
       else
         format.html { render :edit }
