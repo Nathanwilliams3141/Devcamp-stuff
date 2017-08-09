2feac444d060a128fac555ff7b218b7497e38896
Jordan Hudgens <jordan@devcamp.com>
2017-08-09 16:05:17 +0000

    changed website title

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
   
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @page_title = "Nathan Williams Portfolio"
     @seo_keywords = "Nathan Williams portfolio"
   end
 end
